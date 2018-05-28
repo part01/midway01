f59c3eb436d021220ca5248f5424e0a227898a16
greg <veepeenet@outlook.com>
2018-05-28 17:44:18 -0500

    end of second and final prep-to-go-live video (#57)

---
+++ dist/assets/scripts/App.js
@@ -0,0 +1,8 @@
+var Person = require('./modules/Person');
+
+
+var john = new Person("John Doe", "blue-hoo-hoo");
+john.greet();
+
+var jane = new Person("Jane Smith", "greeny-meano");
+jane.greet();
